2b9a41bdfb3b39d38033fe747fba0dbf982ba585
Alex Page <alexanderkpage@gmail.com>
2020-10-01 23:02:12 -0400

    force fail

--- src/main/java/com/app/SpringApp.java
+++ src/main/java/com/app/SpringApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringApp.class, args); /
+		SpringApplication.run(SpringApp.class, args);
 	}
 }
